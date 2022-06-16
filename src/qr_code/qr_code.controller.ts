ca433e62b54aebf32d23188517f43b05aaf3d53b
tamhac2001 <tamhac2001@gmail.com>
2022-06-16 12:57:05 +0700

    return qrcode to browser successfully

--- src/qr_code/qr_code.controller.ts
+++ src/qr_code/qr_code.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { QrCodeService } from './qr_code.service';
 
-@Controller('qr-code')
+@Controller('qrcode')
 export class QrCodeController {
   constructor(private qrCodeService: QrCodeService) {}
 
--- src/qr_code/qr_code.service.ts
+++ src/qr_code/qr_code.service.ts
@@ -8,15 +8,24 @@ import { PassThrough } from 'stream';
 import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class QrCodeService {
+  // @Cron(CronExpression.EVERY_30_SECONDS)
   async getQrCode() {
-    const qrStream = new PassThrough();
+    // const qrStream = new PassThrough();
 
-    const result = await qrcode.toFileStream(qrStream, 'hahahaha', {
-      type: 'png',
-      width: 200,
+    // const result = await qrcode.toFileStream(qrStream, 'hahahaha', {
+    //   type: 'png',
+    //   width: 200,
+    //   errorCorrectionLevel: 'low',
+    // });
+    // qrStream.pipe(result);
+    const graphics = await qrcode.toString(uuidv4(), {
+      type: 'svg',
+      width: 300,
       errorCorrectionLevel: 'low',
+      // margin:,
     });
-    qrStream.pipe(result);
+    console.log(graphics);
+    return graphics;
   }
 
   // @Cron(CronExpression.EVERY_30_SECONDS)
